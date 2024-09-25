8beb960b14ec50f2a9a73c61df43cb23f0c0bad1
xJuanes21 <juanesalazar2004@gmail.com>
2024-09-25 16:26:28 -0500

    Memo hook challenge

---
+++ challenge-memo/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Father } from './components/Father';
+
+function App() {
+  return (
+    <div className="App">
+      <Father />
+    </div>
+  );
+}
+
+export default App;
+
+
