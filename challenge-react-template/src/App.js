fd9f80608e3e8f6079a16a03ee91fda9190e684e
xJuanes21 <juanesalazar2004@gmail.com>
2024-10-19 21:37:40 -0500

    challenge 03

--- challenge-react-template/src/App.js
+++ challenge-react-template/src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-import FirstApp from './components/FirstApp';
-
+import { AddCategory } from './components/AddCategory';
+import { useState } from 'react';
+import { GifGrid } from './components/GifGrid';
 function App() {
+  const [categories, setCategories] = useState(["First Category"])
+
+  const onAddCategory = (category) => {
+    setCategories(list => [...list, category])
+  }
+
   return (
-    <div className="App">
-      <FirstApp />
-    </div>
+    <>
+      <h1>Gif Expert</h1>
+      <AddCategory onAddCategory={onAddCategory} />
+      {
+        categories.map(
+          (category, key) => {
+            return <GifGrid category={category} key={key} />
+          }
+        )
+      }
+    </>
   );
 }
 
---
+++ challenge-react-template/src/components/GifGrid.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const GifGrid = ({ category }) => {
+
+    console.log(category)
+
+    return (
+        <>
+            <h3>{category}</h3>
+            <p>Hello World!</p>
+        </>
+    )
+
+}
